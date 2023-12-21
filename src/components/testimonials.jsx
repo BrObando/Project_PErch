733d0256ba5c9afc4f2d59c9f632483d77ed400c
BrObando <brianna.obando@gmail.com>
2023-12-21 11:44:28 -1000

    Perch

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -2,28 +2,7 @@ import React from 'react';
 import { Box, Typography, Container } from '@mui/material';
 
 const Testimonials = () => {
-  const testimonialsData = [
-    {
-      id: 1,
-      author: 'user',
-      quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec justo vel.',
-    },
-    {
-      id: 2,
-      author: 'user',
-      quote: 'In hac habitasse platea dictumst. Suspendisse potenti. Nullam nec tellus.',
-    },
-    {
-      id: 3,
-      author: 'user',
-      quote: 'Vivamus euismod urna nec neque tincidunt, vitae commodo ex varius.',
-    },
-    {
-      id: 4,
-      author: 'user',
-      quote: 'Integer ut purus sit amet lectus euismod euismod sit amet non mauris.',
-    },
-  ];
+  
 
   return (
     <Container  mt={5}>
