f4a14225e2d2596611e76d0b6e115caf9028ed1f
BrObando <brianna.obando@gmail.com>
2023-12-21 11:28:04 -1000

    deploying

--- src/components/FitnessLevels.jsx
+++ src/components/FitnessLevels.jsx
@@ -5,7 +5,7 @@ import { Container, Typography, Box } from '@mui/material';
 const FitnessLevels = () => {
   return (
     <Container style={{ textAlign: 'center', marginTop: '50px' }}>
-      <Typography variant="h3" mb="40px" fontFamily="Michroma, sans-serif" color="white" mb="73px" mt="30px" style={{ textAlign: 'center' }}>
+      <Typography variant="h3" mb="40px" fontFamily="Michroma, sans-serif" color="white" mt="30px" style={{ textAlign: 'center' }}>
         ALL LEVELS WELCOME
       </Typography>
       <Box display="flex" justifyContent="center">
